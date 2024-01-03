cf79370bc21a4cb5cdf83ed2ff215aec4222d14d
gitesh-vk <gitesh.vk03@gmail.com>
2024-01-03 12:55:14 +0530

    project deployed

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const {PRIVATE_KEY} = process.env;
+module.exports = {
+   solidity: "0.8.20",
+   defaultNetwork: "mumbai",
+   networks: {
+     hardhat: {},
+     mumbai: {
+       url: "https://rpc.ankr.com/polygon_mumbai",
+       accounts: [`0x${PRIVATE_KEY}`],
+     },
+   },
+};
